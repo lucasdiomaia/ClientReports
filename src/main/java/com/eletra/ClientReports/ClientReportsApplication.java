7484ab7383de99b1ea894f97a2b3f39b70d67943
Lucas.Maia <lucas.maia@eletraenergy.com>
2024-03-13 15:40:38 -0300

    ClientReports project initialization

---
+++ src/main/java/com/eletra/ClientReports/ClientReportsApplication.java
@@ -0,0 +1,13 @@
+package com.eletra.ClientReports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientReportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientReportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eletra/ClientReports/ClientReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eletra.ClientReports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientReportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
