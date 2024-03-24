8db7c4f4225d5cc8555546908da29ee238e3e8cb
Lucas.Maia <lucas.maia@eletraenergy.com>
2024-03-24 01:28:26 -0300

    Database connection

--- src/main/java/com/eletra/ClientReports/ClientReportsApplication.java
+++ src/main/java/com/eletra/ClientReports/ClientReportsApplication.java
@@ -1,7 +1,10 @@
 package com.eletra.ClientReports;
 
+import com.eletra.ClientReports.connections.SqlServerConnection;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class ClientReportsApplication {
@@ -9,6 +12,11 @@ public class ClientReportsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ClientReportsApplication.class, args);
 		System.out.println("Starting ClientReports.");
+		try {
+			var connection = SqlServerConnection.getConnection("ClientReportsDB");
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
--- src/main/resources/db.migration/V0_0_1_IntialSchame.sql
+++
@@ -1,9 +0,0 @@
-do $$begin
-
-CREATE TABLE CR_Areas
-(
-    ID   SERIAL PRIMARY KEY,
-    Name VARCHAR(255) NOT NULL
-);
-
-end$$;
